9b6a8db8044541b0f6f0c0a6f78f558e4e41e5fd
Tibidon <goncalves.d.frederic@gmail.com>
2018-06-24 11:05:47 +0200

    Added transition manager script

---
+++ Assets/GameCode/Transition/TransitionManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+namespace WheelchairTrainingGame.Transition
+{
+    public class TransitionManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField]
+        private TransitionScreen[] templates;
+        
+        private TransitionScreen current;
+
+        public bool IsFading
+        {
+            get
+            {
+                if (Visible)
+                    return current.IsFading;
+                else
+                    return false;
+            }
+        }
+
+        public bool Visible
+        {
+            get { return current != null; }
+        }
+
+        private void FindInternalReferences()
+        {
+            templates = GetComponentsInChildren<TransitionScreen>(true);
+        }
+
+        private void OnValidate()
+        {
+            FindInternalReferences();
+        }
+
+        private void Start()
+        {
+            FindInternalReferences();
+        }
+
+        public void Close()
+        {
+            if (Visible)
+                current.GetComponent<TransitionScreen>().Close();
+        }
+
+        public void CompleteClose()
+        {
+            if (Visible)
+            {
+                Destroy(current.gameObject);
+                current = null;
+            }
+        }
+
+        public void Open(TransitionType type)
+        {
+            TransitionScreen screenTemplate = Array.Find(templates, template => template.Type == type);
+            if (screenTemplate != null)
+            {
+                GameObject screen = Instantiate(screenTemplate.gameObject, transform, false);
+                screen.SetActive(true);
+
+                current = screen.GetComponent<TransitionScreen>();
+            }
+        }
+    }
+}
