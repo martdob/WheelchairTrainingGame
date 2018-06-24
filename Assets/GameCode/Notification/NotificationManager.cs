ef0eba03a5365b04c6d6f6f51b55fe522049a601
Tibidon <goncalves.d.frederic@gmail.com>
2018-06-24 11:45:54 +0200

    Added notification scripts

---
+++ Assets/GameCode/Notification/NotificationManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WheelchairTrainingGame.Notification
+{
+    public class NotificationManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField]
+        private NotificationPopup[] templates = new NotificationPopup[3];
+
+        [Header("Options")]
+        [SerializeField]
+        [Range(4, 10)]
+        private int maxNotifications = 8;
+        [SerializeField]
+        private List<NotificationPopup> notifications = new List<NotificationPopup>();
+
+        private void BroadcastLog(string message, string stackTrace, LogType type)
+        {
+            NotificationPopup duplicate = notifications.Find(popup => popup.Type == type && popup.Message == message);
+            if (duplicate != null)
+            {
+                duplicate.Restore();
+            }
+            else
+            {
+                NotificationPopup popupTemplate = Array.Find(templates, template => template.Type == type);
+                if (popupTemplate != null)
+                {
+                    notifications.RemoveAll(item => item == null);
+                    if (notifications.Count >= maxNotifications - 1)
+                    {
+                        notifications[0].Close();
+                    }
+                    
+                    GameObject popupObject = Instantiate(popupTemplate.gameObject, transform, false);
+                    popupObject.SetActive(true);
+
+                    NotificationPopup notification = popupObject.GetComponent<NotificationPopup>();
+                    notification.Message = message;
+
+                    notifications.Add(notification);
+                }
+            }
+        }
+
+        private void FindInternalReferences()
+        {
+            templates = GetComponentsInChildren<NotificationPopup>(true);
+        }
+
+        private void OnValidate()
+        {
+            FindInternalReferences();
+        }
+
+        private void Start()
+        {
+            FindInternalReferences();
+        }
+
+        public void Initialize()
+        {
+            Application.logMessageReceived += BroadcastLog;
+        }
+
+        public void Terminate()
+        {
+            Application.logMessageReceived -= BroadcastLog;
+        }
+    }
+}
---
+++ Assets/GameCode/Notification/NotificationPopup.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WheelchairTrainingGame.Notification
+{
+    [RequireComponent(typeof(Animator))]
+    public class NotificationPopup : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField]
+        private Text text;
+
+        [Header("Notification")]
+        [SerializeField]
+        protected LogType type;
+        [SerializeField]
+        [Range(0.1f, 4.0f)]
+        private float fadeInTimeFactor = 1.0f;
+        [SerializeField]
+        [Range(0.1f, 4.0f)]
+        private float fadeOutTimeFactor = 1.0f;
+        [SerializeField]
+        private float duration = 4.0f;
+        [SerializeField]
+        private string message;
+
+        private Animator animator;
+        private bool isFading = true;
+
+        public LogType Type
+        {
+            get { return type; }
+        }
+
+        public bool IsFading
+        {
+            get { return isFading; }
+        }
+
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                message = value;
+                if (text != null)
+                    text.text = message;
+            }
+        }
+
+        private IEnumerator DelayDuration()
+        {
+            yield return new WaitForSeconds(duration);
+
+            Close();
+        }
+
+        private void FindInternalReferences()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void OnValidate()
+        {
+            FindInternalReferences();
+            isFading = true;
+        }
+
+        private void OnWindowClose()
+        {
+            isFading = false;
+            Destroy(gameObject);
+        }
+
+        private void OnWindowOpen()
+        {
+            isFading = false;
+            StartCoroutine(DelayDuration());
+        }
+
+        private void Start()
+        {
+            FindInternalReferences();
+            Open();
+        }
+
+        public void Close()
+        {
+            StopAllCoroutines();
+
+            isFading = true;
+            animator.SetFloat("CloseTimeFactor", fadeOutTimeFactor);
+            animator.SetTrigger("Close");
+        }
+
+        public void Open()
+        {
+            isFading = true;
+            animator.SetFloat("OpenTimeFactor", fadeInTimeFactor);
+            animator.SetTrigger("Open");
+        }
+
+        public void Restore()
+        {
+            StopAllCoroutines();
+            StartCoroutine(DelayDuration());
+        }
+    }
+}
