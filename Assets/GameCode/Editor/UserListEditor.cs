53dbda792d00121dfb5745699170ac5fdd7c328d
Tibidon <goncalves.d.frederic@gmail.com>
2018-07-07 16:46:00 +0200

    Added user list editor window

---
+++ Assets/GameCode/Editor/UserListEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using WheelchairTrainingGame.User;
+
+public class UserListEditor : EditorWindow
+{
+    [MenuItem("Assets/Create/WheelchairTrainingGame/User List")]
+    public static UserList Create()
+    {
+        UserList asset = ScriptableObject.CreateInstance<UserList>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/UserList.asset");
+        AssetDatabase.SaveAssets();
+
+        return asset;
+    }
+}
