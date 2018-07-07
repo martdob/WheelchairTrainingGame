16bc5131b43296d49bada12bf8da7068df5d8d04
Tibidon <goncalves.d.frederic@gmail.com>
2018-07-07 16:34:18 +0200

    Added user list (empty)

---
+++ Assets/GameCode/User/UserList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WheelchairTrainingGame.User
+{
+    public class UserList : ScriptableObject
+    {
+        public List<UserModel> Users = new List<UserModel>();
+    }
+}
