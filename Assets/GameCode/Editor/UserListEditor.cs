a4c1bbd864f22676725cb7059450257431b08d0d
Tibidon <goncalves.d.frederic@gmail.com>
2018-07-08 15:15:14 +0200

    Added editor methods to populate and delete user list

--- Assets/GameCode/Editor/UserListEditor.cs
+++ Assets/GameCode/Editor/UserListEditor.cs
@@ -2,16 +2,53 @@
 using UnityEngine;
 using WheelchairTrainingGame.User;
 
-public class UserListEditor : EditorWindow
+[CustomEditor(typeof(UserList))]
+public class UserListEditor : Editor
 {
     [MenuItem("Assets/Create/WheelchairTrainingGame/User List")]
     public static UserList Create()
     {
-        UserList asset = ScriptableObject.CreateInstance<UserList>();
+        UserList asset = CreateInstance<UserList>();
 
         AssetDatabase.CreateAsset(asset, "Assets/UserList.asset");
         AssetDatabase.SaveAssets();
 
         return asset;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        // Get editor target
+        UserList userList = (UserList)target;
+
+        // Show default inspector property editor
+        DrawDefaultInspector();
+
+        GUILayout.Space(20);
+
+        GUILayout.BeginVertical();
+
+        if (GUILayout.Button("Random"))
+        {
+            UserModel[] newUsers = new UserModel[] {
+                new UserModel { Gender = Gender.Male, Name = "Paul" },
+                new UserModel { Gender = Gender.Female, Name = "Alice" },
+                new UserModel { Gender = Gender.Male, Name = "Jean" },
+                new UserModel { Gender = Gender.Female, Name = "Tamara" },
+                new UserModel { Gender = Gender.Male, Name = "Pierre" }
+            };
+
+            userList.Users.AddRange(newUsers);
+        }
+
+        if(GUILayout.Button("DeleteAll"))
+        {
+            int userCount = userList.Users.Count;
+            userList.Users.Clear();
+
+            Debug.LogFormat("Deleted {0} users from list.", userCount);
+        }
+
+        GUILayout.EndVertical();
     }
 }
--- Assets/GameCode/User/UserModel.cs
+++ Assets/GameCode/User/UserModel.cs
@@ -3,7 +3,7 @@
     [System.Serializable]
     public class UserModel
     {
-        public Gender Gender;
         public string Name;
+        public Gender Gender;
     }
 }
