46fc778e5a900039af632c0528f3c520948f78bd
Tibidon <goncalves.d.frederic@gmail.com>
2018-07-08 15:34:09 +0200

    Added user list widget

---
+++ Assets/GameCode/User/UserListWidget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace WheelchairTrainingGame.User
+{
+    public class UserListWidget : MonoBehaviour
+    {
+        [SerializeField]
+        private UserList userList;
+
+        [SerializeField]
+        private GameObject femaleTemplate;
+        [SerializeField]
+        private GameObject maleTemplate;
+
+        public List<UserWidget> widgets = new List<UserWidget>();
+
+        private void Start()
+        {
+            foreach(UserModel model in userList.Users)
+            {
+                GameObject userObject = Instantiate(model.Gender == Gender.Female ? femaleTemplate : maleTemplate, transform, false);
+                userObject.SetActive(true);
+
+                UserWidget userWidget = userObject.GetComponent<UserWidget>();
+                userWidget.SetUserModel(model);
+                widgets.Add(userWidget);
+            }
+
+
+            EventSystem.current.SetSelectedGameObject(widgets[0].gameObject);
+        }
+    }
+}
---
+++ Assets/GameCode/User/UserWidget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace WheelchairTrainingGame.User
+{
+    public class UserWidget : MonoBehaviour
+    {
+        [SerializeField]
+        private Text nameText;
+        [SerializeField]
+        private UserModel model;
+        
+        private void OnValidate()
+        {
+            if (nameText == null)
+            {
+                nameText = GetComponentInChildren<Text>();
+            }
+        }
+
+        public void SetUserModel(UserModel user)
+        {
+            model = user;
+            nameText.text = model.Name;
+        }
+    }
+}
